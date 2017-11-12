6c20caa3b128941f5a5b3cc1f9bdc38cca9863ec
rocker <rockerbear027@gmail.com>
2017-11-12 15:05:28 +0800

    1. addActivity insert data to sqlite 2. create class ShowToast

--- app/src/main/java/com/coors/expensestudy/AddActivity.java
+++ app/src/main/java/com/coors/expensestudy/AddActivity.java
@@ -1,15 +1,65 @@
 package com.coors.expensestudy;
 
+import android.content.ContentValues;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class AddActivity extends AppCompatActivity {
+public class AddActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText etCdate;
+    private EditText etInfo;
+    private EditText etAmount;
+    private Button btnAdd;
+    private Button btnCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+        findViews();
+    }
 
+    private void findViews() {
+        etCdate = findViewById(R.id.add_et_date);
+        etInfo = findViewById(R.id.add_et_info);
+        etAmount = findViewById(R.id.add_et_amount);
+        btnAdd = findViewById(R.id.add_btn_add);
+        btnCancel = findViewById(R.id.add_btn_cancel);
+        btnAdd.setOnClickListener(this);
+        btnCancel.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.add_btn_add:
+                doVerification();
+                break;
+            case R.id.add_btn_cancel:
+                GoToActivity.goTo(this,GoToActivity.HOME_MAIN_ACTIVITY);
+                break;
+        }
+    }
+
+    private void doVerification() {
+        if (etCdate.getText().toString().trim().matches("") && etInfo.getText().toString().trim().matches("") && etAmount.getText().toString().trim().matches("")) {
+            ShowToast.getInstance(this).show("欄位均不可為空值");
+        } else {
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(ExpenseHelper.TABLE_SCHEMA.COL_CDATE,etCdate.getText().toString());
+            contentValues.put(ExpenseHelper.TABLE_SCHEMA.COL_INFO,etInfo.getText().toString());
+            contentValues.put(ExpenseHelper.TABLE_SCHEMA.COL_AMOUNT,etAmount.getText().toString());
+            long id = ExpenseHelper.getInstance(this).getWritableDatabase().insert(ExpenseHelper.TABLE_SCHEMA.TABLE_NAME, null, contentValues);
+            if (id != -1) {
+                ShowToast.getInstance(this).show("新增成功");
+            } else {
+                ShowToast.getInstance(this).show("新增失敗");
+            }
+
+        }
     }
 }
---
+++ app/src/main/java/com/coors/expensestudy/ShowToast.java
@@ -0,0 +1,36 @@
+package com.coors.expensestudy;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by z8v on 2017/11/12.
+ */
+
+public class ShowToast {
+    private Toast toast;
+    static ShowToast instance;
+    private Context mContext;
+    public static final int DURATION =Toast.LENGTH_SHORT;
+
+    public ShowToast(Context context) {
+        mContext = context;
+    }
+
+    public static ShowToast getInstance(Context context) {
+        if (instance == null) {
+            instance = new ShowToast(context);
+        }
+        return instance;
+    }
+
+    public void show(String msg) {
+        if (toast == null) {
+            toast = android.widget.Toast.makeText(mContext, msg, DURATION);
+        } else {
+            toast.setText(msg);
+            toast.setDuration(DURATION);
+        }
+        toast.show();
+    }
+}
